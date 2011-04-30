c87de770ebcf20bd610c6e31510bf40130daf201
Ben Scholz <bbscholz@gmail.com>
2011-04-30 17:49:50 -0500

    updated test to reflect remove()'s new return value

--- test/tests.js
+++ test/tests.js
@@ -4,7 +4,7 @@ test('jaylist', function() {
     equals(jaylist.add("this", 1), 1, 'Added ("that", 1).');
     equals(jaylist.get("this"), 1, 'Retrieved the value of "that".');
     equals(jaylist.get("that"), undefined, 'Cannot retrieve value that does not exist.');
-    ok(jaylist.remove("this"), 'Removed "this".');
+    equals(jaylist.remove("this"), 1, 'Removed "this".');
     ok(!jaylist.remove("that"), 'Cannot remove value that does not exist.');
     equals(jaylist.add("this", 4), 4, 'Added ("this", 4).');
     equals(jaylist.add("this", 7), 7, 'Overwrite ("this", 4) with ("this", 7).');
