92c13b7cef8c690b64240f837ba38eccd7b97150
Ben Scholz <bbscholz@gmail.com>
2012-05-10 21:07:03 -0500

    much needed cleanup of tests

--- test/tests.js
+++ test/tests.js
@@ -1,224 +1,306 @@
-var jaylist = list(),
-otherlist = list(),
-copylist = list();
 
-otherlist.add("other", "list");
 
-module("jaylist Module");
+module('jaylist module');
 
-test("create lists", function() {
-	expect(3);
+test('create a list', function () {
+		expect(1);
+
+		var xs = list();
+		ok(xs, 'list created');
+});
+
+test('add: add items to a list', function () {
+		expect(6);
+
+		var xs = list();
+		var ys = list();
+		
+		deepEqual(xs.add('string', 'strval'), 'strval', 'Added ("string", "strval").');
+		deepEqual(xs.add('number', 18593214), 18593214, 'Added ("number", 18593214).');
+		deepEqual(xs.add('boolean',   false),    false, 'Added ("boolean",   false).');
+		deepEqual(xs.add('object',	     ys),       ys, 'Added ("object",       ys).');
+		deepEqual(xs.add('array', [1,2,3,4]), [1,2,3,4],'Added ("array", [1,2,3,4]).');
+		deepEqual(xs.add('array', [4,3,2,1]), [4,3,2,1],'Added ("array", [4,3,2,1]).');
+});
+
+test('hasKey: check a list for keys', function () {
+		expect(6);
+
+		var xs = list();
+		var ys = list();
+
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		ok(xs.hasKey('string'), 'Has key "string".');
+		ok(xs.hasKey('number'), 'Has key "number".');
+		ok(xs.hasKey('boolean'),'Has key "boolean".');
+		ok(xs.hasKey('object'), 'Has key "object".');
+		ok(xs.hasKey('array'),  'Has key "array".');
+		ok(!xs.hasKey('missing'),'Missing key "missing".');
+});
+
+test('get: retrieve values using a key', function () {
+		expect(5);
+		
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.get('string'), 'strval', 'Retrieved the value of "string".');
+		deepEqual(xs.get('number'), 18593214, 'Retrieved the value of "number".');
+		deepEqual(xs.get('boolean'),false,    'Retrieved the value of "boolean".');
+		deepEqual(xs.get('object'), ys,       'Retrieved the value of "object".');
+		deepEqual(xs.get('array'),  [1,2,3,4],'Retrieved the value of "array".');
+});
+
+test('remove: remove values using a key', function () {
+		expect(10);
+		
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.remove('string'), 'strval', 'Removed the value of "string".');
+		ok(!xs.get('string'), '"string" removed.');
+
+		deepEqual(xs.remove('number'), 18593214, 'Removed the value of "number".');
+		ok(!xs.get('number'), '"number" removed.');
+
+		deepEqual(xs.remove('boolean'),false,    'Removed the value of "boolean".');
+		ok(!xs.get('boolean'), '"boolean" removed.');
+
+		deepEqual(xs.remove('object'), ys,       'Removed the value of "object".');
+		ok(!xs.get('object'), '"object" removed.');
+
+		deepEqual(xs.remove('array'),  [1,2,3,4],'Removed the value of "array".');
+		ok(!xs.get('array'), '"array" removed.');
+});
+
+
+test('keys: return the keys in the list', function () {
+		expect(1);
+		
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.keys(), ['string', 'number', 'boolean', 'object', 'array'], 'Popped the value of "string".');
+});
+
+test('values: return values in the list, function () {
+		expect(1);
+		
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.values(), ['strval', 18593214, false, ys, [1,2,3,4]], 'Popped the value of "string".');
+});
+
+test('items: return (key, value) pairs in the list, function () {
+		expect(1);
+		
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.values(), [['string', 'strval'], ['number', 18593214],
+														['boolean',   false], ['object',       ys], 
+														['array', [1,2,3,4]]], 'Popped the value of "string".');
+});
+
+test('len: return the number of items in the list', function () {
+		expect(2);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		equal(xs.len(), 5, 'List of length 5.');
+		equal(ys.len(), 0, 'List of length 0.');
+});
+
+test('clear: empty the list', function () {
+		expect(2);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		equal(xs.len(), 5, 'List of length 5.');
+		xs.clear();
+		equal(xs.len(), 0, 'List of length 0.');
+});
+
+test('update: add the contents of one list to another', function () {
+		expect(1);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		ys.update(xs);
+		deepEqual(xs, ys, 'List updated.');
+});
+
+test('copy: return a deep copy of the list', function () {
+		expect(1);
+
+		var ws;
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		ws = xs.copy();
+		deepEqual(xs, ws, 'List copied.');
+});
+
+test('each: execute a callback for each key in the list', function () {
+		expect(1);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		xs.each(xs.remove);
+		equal(xs.len(), 0, 'Callback `remove` executed.');
+});
+
+test('next: loop-style iteration', function () {
+		expect(5);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.next(),  'strval', '`string` retrieved.');
+		deepEqual(xs.next(),  18593214, '`number` retrieved.');
+		deepEqual(xs.next(),     false, '`boolean` retrieved.');
+		deepEqual(xs.next(),        ys, '`object` retrieved.');
+		deepEqual(xs.next(), [1,2,3,4], '`array` retrieved.');
+		deepEqual(xs.next(), undefined, 'iteration complete.');
+});
+
+test('object: returns the list in object-literal form', function () {
+		expect(1);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		deepEqual(xs.object(),  {'string' : 'strval',
+														 'number' : 18593214,
+														 'boolean': false,
+														 'object' : ys,
+														 'array'  : [1,2,3,4]}, 'Object retrieved.');
+});
+
+test('isEqual: list equality', function () {
+		expect(1);
+
+		var xs = list();
+		var ys = list();
+		
+		xs.add('string', 'strval');
+		xs.add('number', 18593214);
+		xs.add('boolean,    false);
+		xs.add('object',       ys);
+		xs.add('array', [1,2,3,4]);
+
+		ys.add('string', 'strval');
+		ys.add('number', 18593214);
+		ys.add('boolean,    false);
+		ys.add('object',       ys);
+		ys.add('array', [1,2,3,4]);
 	
-	var ceelist,
-	beelist = list();
-	ok(beelist, "create a list");
-	beelist.add("one", 1);
-	beelist.add("two", 2);
-	beelist.add("three", 3);
-	ceelist = beelist.copy();
-	ok(beelist.isEqual(ceelist), "list is copied");
-	beelist.clear();
-	equal(ceelist.get("one"), 1, "list is deep copied");
-});
-
-test("add", function() {
-    expect(6);
-    
-    equal(jaylist.add("string", "string value"), "string value", 'Added ("string", "string value").');
-    equal(jaylist.add("number", 4), 4, 'Added ("number", 4).');
-    equal(jaylist.add("boolean", false), false, 'Added ("boolean", true).');
-    deepEqual(jaylist.add("array", [1,2,3]), [1,2,3], 'Added ("array", [1,2,3]).');
-    equal(jaylist.add("object", otherlist), otherlist, 'Added ("object", otherlist).');
-    deepEqual(jaylist.add("array", [4,5,6]), [4,5,6], 'Overwrite ("array", [1,2,3]) with ("array", [4,5,6]).');
-});
-
-test("hasKey", function() {
-    expect(6);
-    
-    equal(jaylist.hasKey("string"), true, 'Has key "string".');
-    equal(jaylist.hasKey("number"), true, 'Has key "number".');
-    equal(jaylist.hasKey("boolean"), true, 'Has key "boolean".');
-    equal(jaylist.hasKey("array"), true, 'Has key "array".');
-    equal(jaylist.hasKey("object"), true, 'Has key "object".');
-    equal(jaylist.hasKey("missing"), false, 'Missing key "missing".');
-});
-
-test("get", function() {
-    expect(6);
-    
-    equal(jaylist.get("string"), "string value", 'Retrieved the value of "string" ("string value").');
-    equal(jaylist.get("number"), 4, 'Retrieved the value of "number" (4).');
-    equal(jaylist.get("boolean"), false, 'Retrieved the value of "boolean" (false).');
-    deepEqual(jaylist.get("array"), [4,5,6], 'Retrieved the value of "array" ([4,5,6]).');
-    equal(jaylist.get("object"), otherlist, 'Retrieved the value of "object" (otherlist).');
-    equal(jaylist.get("missing"), undefined, 'Cannot retrieve a value not in the list (undefined).');
+		ok(isEqual(xs, ys), 'Lists are equal.');
 });
 
 
-test("remove", function() {
-    expect(31);
-    
-    copylist = jaylist.copy();
 
-    equal(jaylist.get("string"), "string value", 'Retrieved the value of "string" ("string value").');
-    jaylist.remove("string");
-    equal(jaylist.get("string"), undefined, 'Removed "string".');
-    
-    equal(jaylist.get("number"), 4, 'Retrieved the value of "number" (4).');
-    jaylist.remove("number");
-    equal(jaylist.get("number"), undefined, 'Removed "number".');
 
-    equal(jaylist.get("boolean"), false, 'Retrieved the value of "boolean" (false).');
-    jaylist.remove("boolean");
-    equal(jaylist.get("boolean"), undefined, 'Removed "boolean".');
 
-    deepEqual(jaylist.get("array"), [4,5,6], 'Retrieved the value of "array" ([4,5,6]).');
-    jaylist.remove("array");
-    equal(jaylist.get("array"), undefined, 'Removed "array".');
 
-    equal(jaylist.get("object").table, otherlist.table, 'Retrieved the value of "object" (otherlist).');
-    jaylist.remove("object");
-    equal(jaylist.get("object"), undefined, 'Removed "object".');
-    equal(jaylist.remove("missing"), undefined, 'Cannot remove value not in the list.');
 
-    jaylist.update(copylist);
-    equal(jaylist.get("string"), "string value", 'Retrieved the value of "string" ("string value").');
-    equal(jaylist.get("number"), 4, 'Retrieved the value of "number" (4).');
-    equal(jaylist.get("boolean"), false, 'Retrieved the value of "boolean" (false).');
-    deepEqual(jaylist.get("array"), [4,5,6], 'Retrieved the value of "array" ([4,5,6]).');
-    equal(jaylist.get("object").table, otherlist.table, 'Retrieved the value of "object" (otherlist).');
-	jaylist.remove(["string","number","boolean","array","object"]);
-    equal(jaylist.get("string"), undefined, 'Removed "string".');
-    equal(jaylist.get("number"), undefined, 'Removed "number".');
-    equal(jaylist.get("boolean"), undefined, 'Removed "boolean".');
-    equal(jaylist.get("array"), undefined, 'Removed "array".');
-    equal(jaylist.get("object"), undefined, 'Removed "object".');
-    
-	jaylist.update(copylist);
-    equal(jaylist.get("string"), "string value", 'Retrieved the value of "string" ("string value").');
-    equal(jaylist.get("number"), 4, 'Retrieved the value of "number" (4).');
-    equal(jaylist.get("boolean"), false, 'Retrieved the value of "boolean" (false).');
-    deepEqual(jaylist.get("array"), [4,5,6], 'Retrieved the value of "array" ([4,5,6]).');
-    equal(jaylist.get("object").table, otherlist.table, 'Retrieved the value of "object" (otherlist).');
-    jaylist.remove(copylist);
-    equal(jaylist.get("string"), undefined, 'Removed "string".');
-    equal(jaylist.get("number"), undefined, 'Removed "number".');
-    equal(jaylist.get("boolean"), undefined, 'Removed "boolean".');
-    equal(jaylist.get("array"), undefined, 'Removed "array".');
-    equal(jaylist.get("object"), undefined, 'Removed "object".');
-});
 
-test("pop", function () {
-    expect(11);
 
-    jaylist.update(copylist);
-    equal(jaylist.get("string"), "string value", 'Retrieved the value of "string" ("string value").');
-    jaylist.pop("string");
-    equal(jaylist.get("string"), undefined, 'Popped key.');
-    
-    equal(jaylist.get("number"), 4, 'Retrieved the value of "number" (4).');
-    jaylist.pop("number");
-    equal(jaylist.get("number"), undefined, 'Popped key.');
-    
-    equal(jaylist.get("boolean"), false, 'Retrieved the value of "boolean" (false).');
-    jaylist.pop("boolean");
-    equal(jaylist.get("boolean"), undefined, 'Popped key.');
-    
-    deepEqual(jaylist.get("array"), [4,5,6], 'Retrieved the value of "array" ([4,5,6]).');
-    jaylist.pop("array");
-    equal(jaylist.get("array"), undefined, 'Popped key.');
-    
-    jaylist.add("object", otherlist);
-    equal(jaylist.get("object").table, otherlist.table, 'Retrieved the value of "object" (otherlist).');
-    jaylist.pop("object");
-    equal(jaylist.get("object"), undefined, 'Popped key.');
-    
-    equal(jaylist.pop("Missing"), undefined, 'Popping missing key returns undefined.');
-});
 
-test("popItem", function () {
-    expect(2);
-    
-    jaylist.add("pop", "item");
-    ok(jaylist.hasKey("pop"), 'Popped a random item.');
-    var popped = jaylist.popItem();
-    ok(!jaylist.hasKey("pop"), 'Popped item removed from list.');
-});
 
-test("keys, values, items", function() {
-    expect(3);
-    jaylist.update(copylist);
-    deepEqual(jaylist.keys(), ['string','number','boolean','array','object'], 'Retrieved keys.');
-    deepEqual(jaylist.values(), ['string value',4,false,[4,5,6],copylist.object().object], 'Retrieved values.');
-    deepEqual(jaylist.items(), [['string','string value'],['number',4],['boolean',false],['array',[4,5,6]],['object',copylist.object().object]], 'Retrieved items.');
-});
 
-test("len", function() {
-    expect(2);
-    equal(jaylist.len(), 5, 'Retrieved length.');
-    var emptylist = list();
-    equal(emptylist.len(), 0, 'Retrieved empty list length.')
-});
 
-test("clear", function() {
-    expect(2);
-    
-    equal(jaylist.len(), 5, 'Non-zero length retrieved.')
-    jaylist.clear();
-    equal(jaylist.len(), 0, 'Cleared list.');
-});
 
-test("update", function() {
-    expect(1);
-    
-    jaylist.update(otherlist);
-    equal(jaylist.keys().toString(), "other", 'Updated list with list.');
-});
 
-test("copy", function() {
-    expect(2);
 
-    jaylist = copylist.copy();
-    deepEqual(jaylist.keys(), ['string','number','boolean','array','object'], 'Retrieved keys.');
-    deepEqual(jaylist.values(), ['string value',4,false,[4,5,6],jaylist.get('object')], 'Retrieved values.');
-});
 
-test("each", function() {
-    expect(2);
-    
-    equal(jaylist.len(), 5, "list of length 5.");
-    jaylist.each(function(key){jaylist.pop(key)});
-    equal(jaylist.len(), 0, "Popped each item in the list");
-});
 
-test("next", function() {
-    expect(3);
-    
-    var alist = list();
-    alist.add("this", "that");
-    alist.add("us", "them");
-    ok(alist.hasKey(alist.next()));
-    ok(alist.hasKey(alist.next()));
-    equal(alist.next(), undefined);
-});
 
-test("object", function() {
-    expect(2);
-    
-    var listy = list()
-    listy.add("key1", "value1");
-    listy.add("key2", "value2");
-    var obj = listy.object();
-    equal(obj['key1'], "value1", "Retrieved value1.");
-    equal(obj['key2'], "value2", "Retrieved value2.");
-});
 
-test("isEqual", function() {
-    expect(4);
-    
-    jaylist.update(copylist);
-    ok(jaylist.isEqual(copylist), "lists are equal");
-    ok(copylist.isEqual(jaylist), "lists are equal");
-    ok(!jaylist.isEqual([1,2,3]), "list is not equal to an array");
-    ok(!jaylist.isEqual(otherlist), "list is not equal to list");
-});
+
+
+
+
+
